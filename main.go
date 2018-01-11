c5b8cc9f045b7002944ca332c8eddd80b17f54c5
YujiYabe <yayuji@gmail.com>
2018-01-12 08:29:13 +0900

    o

--- main.go
+++ main.go
@@ -7,13 +7,12 @@ import (
     "os"
     "runtime"
     "log"
-	"encoding/json"
     "time"
-    "strconv"
     "github.com/labstack/echo"
     "github.com/labstack/echo/middleware"
     "github.com/jinzhu/gorm"
     _ "github.com/mattn/go-sqlite3"
+
     "github.com/skratchdot/open-golang/open"
 
 )
@@ -47,7 +46,7 @@ type Setting struct {
 
 
 
-// Note オンラインストレージのノート
+// Note ページの内容
 type Note struct {
     gorm.Model
     PageTitle   string `json:"page_title"`
@@ -112,17 +111,17 @@ func main() {
 
     // 各ルーティングに対するハンドラを設定
 
+    e.GET("/", HandleLoadPageGet)
     e.POST("/addnote", HandleAddNotePost)
     e.POST("/addpage", HandleAddPagePost)
 
     e.POST("/updatenote", HandleUpdateNotePost)
     e.POST("/updatepage", HandleUpdatePagePost)
 
-    e.POST("/deletepage", HandleDeletePagePost)
     e.POST("/deletenote", HandleDeleteNotePost)
+    e.POST("/deletepage", HandleDeletePagePost)
 
     e.GET("/livecheck", HandleLiveCheckGet)
-    e.GET("/", HandleLoadPageGet)
  
 
     open.Run("http://localhost:" + usePortNumber)
@@ -133,6 +132,123 @@ func main() {
     // サーバーを開始
     e.Logger.Fatal(e.Start(":" + usePortNumber))
 } //--------------------------------------------
+
+
+// HandleLoadPageGet のコメントアウト
+func HandleLoadPageGet(c echo.Context) error {
+
+    returnjson := getData()
+
+    return c.Render(http.StatusOK, "loadpage", returnjson)
+
+} //--------------------------------------------
+
+//HandleAddNotePost は /hello のPost時のHTMLデータ生成処理を行います。
+func HandleAddNotePost(c echo.Context) error {
+    log.Println("______________________________________________________________________________" )
+    log.Println("ノート追加 開始")
+
+    argMap := make(map[string]string)
+    argMap["newNoteName"]    = c.FormValue("new_note_name")
+    argMap["newNoteAddress"] = c.FormValue("new_note_address")
+
+    addNote(argMap)
+
+    // タスク パスが存在するかの確認
+    log.Println("ノート追加 終了")
+    log.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" )
+    return nil
+    
+} //--------------------------------------------
+
+//HandleAddPagePost は /hello のPost時のHTMLデータ生成処理を行います。
+func HandleAddPagePost(c echo.Context) error {
+    log.Println("______________________________________________________________________________" )
+    log.Println("ページ追加 開始")
+
+    argMap := make(map[string]string)
+    argMap["noteAddress"] = c.FormValue("note_address")
+
+    addPage(argMap)
+
+    log.Println("ページ追加 終了")
+    log.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" )
+
+    return nil
+
+} //--------------------------------------------
+
+
+//HandleUpdateNotePost は /hello のPost時のHTMLデータ生成処理を行います。
+func HandleUpdateNotePost(c echo.Context) error {
+    log.Println("______________________________________________________________________________" )
+    log.Println("ノート更新 開始")
+
+    argMap := make(map[string]string)
+    argMap["newNoteName"] = c.FormValue("new_note_name")
+    argMap["postNoteID"] = c.FormValue("note_id")
+    updateNote(argMap)
+
+    log.Println("ノート更新 終了")
+    log.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" )
+
+    return nil
+} //--------------------------------------------
+
+//HandleUpdatePagePost は /hello のPost時のHTMLデータ生成処理を行います。
+func HandleUpdatePagePost(c echo.Context) error {
+    log.Println("______________________________________________________________________________" )
+    log.Println("ページ更新 開始")
+
+    argMap := make(map[string]string)
+    argMap["noteAddress"] = c.FormValue("note_address")
+    argMap["pageID"]      = c.FormValue("page_id")
+    argMap["pageTitle"]   = c.FormValue("page_title")
+    argMap["pageBody"]    = c.FormValue("page_body")
+
+    updatePage(argMap)
+
+
+    log.Println("ページ更新 終了")
+    log.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" )
+
+    return nil
+} //--------------------------------------------
+
+// HandleDeletePagePost のコメントアウト
+func HandleDeletePagePost(c echo.Context) error {
+    log.Println("______________________________________________________________________________" )
+    log.Println("ページ削除 開始")
+
+
+    argMap := make(map[string]string)
+    argMap["noteAddress"] = c.FormValue("note_address")
+    argMap["pageID"]      = c.FormValue("page_id")
+
+    deletePage(argMap)
+    
+    log.Println("ページ削除 終了")
+    log.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" )
+
+    return nil
+
+} //--------------------------------------------
+
+//HandleDeleteNotePost は /hello のPost時のHTMLデータ生成処理を行います。
+func HandleDeleteNotePost(c echo.Context) error {
+    log.Println("______________________________________________________________________________" )
+    log.Println("ノート削除 開始")
+
+    argMap := make(map[string]string)
+    argMap["postNoteID"] = c.FormValue("note_id")
+
+    log.Println("ノート削除 終了")
+    log.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" )
+
+    return nil
+} //--------------------------------------------
+
+
 
 // HandleLiveCheckGet のコメントアウト
 func HandleLiveCheckGet(c echo.Context) error {
@@ -153,298 +269,6 @@ func HandleLiveCheckGet(c echo.Context) error {
     return nil
 } //--------------------------------------------
 
-// HandleDeletePagePost のコメントアウト
-func HandleDeletePagePost(c echo.Context) error {
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-    log.Println("ページ削除 開始")
-
-	// __________________________________
-	// ポスト内容取得
-    noteAddress := c.FormValue("note_address")
-    pageID      := c.FormValue("page_id")
-
-    notedb, err := gorm.Open("sqlite3", noteAddress + directorySeparator + noteDBName )
-    if err != nil {
-        panic("failed to connect database")
-    }
-
-    defer notedb.Close()
-	notedb.LogMode(true)
-
-	// __________________________________
-	// DB内容取得
-    notedb.Where("id = ?", pageID).Delete(&Note{})
-    
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-
-    log.Println("ページ削除 終了")
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-
-    return nil
-
-} //--------------------------------------------
-
-
-// HandleUpdateNoteFromPage のコメントアウト
-func HandleUpdateNoteFromPage(noteAddress string)  {
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-    log.Println("ノート更新日時更新 開始")
-
-
-    settingdb, err := gorm.Open("sqlite3", settingDBName )
-    defer settingdb.Close()
-	settingdb.LogMode(true)
-
-    if err != nil {
-        panic("failed to connect database")
-    }
-
-    var setting Setting
-
-    settingdb.Where("address = ?", noteAddress ).First(&setting)
-
-    setting.Address = noteAddress
-    settingdb.Save(&setting)
-
-
-    log.Println("ノート更新日時更新 終了")
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-
-} //--------------------------------------------
-
-//HandleUpdatePagePost は /hello のPost時のHTMLデータ生成処理を行います。
-func HandleUpdatePagePost(c echo.Context) error {
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-    log.Println("ページ更新 開始")
-
-	// __________________________________
-	// ポスト内容取得
-    noteAddress := c.FormValue("note_address")
-    pageID      := c.FormValue("page_id")
-    pageTitle   := c.FormValue("page_title")
-    pageBody    := c.FormValue("page_body")
-
-    if pageTitle == "" {
-        pageTitle =  " "
-    }
-
-    if pageBody == "" {
-        pageBody =  " "
-    }
-    
-    notedb, err := gorm.Open("sqlite3", noteAddress + directorySeparator + noteDBName )
-    if err != nil {
-        panic("failed to connect database")
-    }
-
-    defer notedb.Close()
-	notedb.LogMode(true)
-
-
-	// __________________________________
-	// DB内容取得
-    NoteList := []Note{}
-
-	notedb.Model(&NoteList).Where("id = ?", pageID).Update(&Note{PageTitle: pageTitle, PageBody: pageBody})
-
-
-    
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-    HandleUpdateNoteFromPage(noteAddress)
-
-    log.Println("ページ更新 終了")
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-
-    return nil
-} //--------------------------------------------
-
-
-//HandleUpdateNotePost は /hello のPost時のHTMLデータ生成処理を行います。
-func HandleUpdateNotePost(c echo.Context) error {
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-    log.Println("ノート更新 開始")
-
-	// __________________________________
-	// ポスト内容取得
-    newNoteName := c.FormValue("new_note_name")
-    postNoteID  := c.FormValue("note_id")
-
-    
-    //------------------------------
-    settingdb, err := gorm.Open( useDBMSName , settingDBName )
-    defer settingdb.Close()
-	settingdb.LogMode(true)
-
-    if err != nil {
-        panic("failed to connect database2")
-    }
-
-    var setting Setting
-    // noteID := int(noteID)
-    var noteID int
-    noteID, _ = strconv.Atoi(postNoteID)
-
-    settingdb.Where("id = ?", noteID ).First(&setting)
-
-    setting.Name = newNoteName
-    settingdb.Save(&setting)
-
-
-
-    log.Println("ノート更新 終了")
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-
-    return nil
-} //--------------------------------------------
-
-
-
-//HandleDeleteNotePost は /hello のPost時のHTMLデータ生成処理を行います。
-func HandleDeleteNotePost(c echo.Context) error {
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-    log.Println("ノート削除 開始")
-
-	// __________________________________
-	// ポスト内容取得
-    postNoteID  := c.FormValue("note_id")
-
-    //------------------------------
-    settingdb, err := gorm.Open( useDBMSName , settingDBName )
-    defer settingdb.Close()
-	settingdb.LogMode(true)
-
-    if err != nil {
-        panic("failed to connect database")
-    }
-
-    var setting Setting
-    var noteID int
-    noteID, _ = strconv.Atoi(postNoteID)
-
-    settingdb.Where("id = ?", noteID ).Delete(&setting)
-
-    log.Println("ノート削除 終了")
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-
-    return nil
-} //--------------------------------------------
-
-
-
-
-//HandleAddNotePost は /hello のPost時のHTMLデータ生成処理を行います。
-func HandleAddNotePost(c echo.Context) error {
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-    log.Println("ノート追加 開始")
-
-    //指定したディレクトリにDBファイルを作成
-    newNoteName    := c.FormValue("new_note_name")
-    newNoteAddress := c.FormValue("new_note_address")
-
-    // タスク パスが存在するかの確認
-
-
-    notefullAddress := newNoteAddress + directorySeparator + noteDBName
-
-    file, err := os.OpenFile( notefullAddress , os.O_WRONLY|os.O_CREATE, 0666 )
-    if err != nil {
-        //エラー処理
-        log.Fatal(err)
-    }
-    defer file.Close()
-
-    notedb, err := gorm.Open( useDBMSName , notefullAddress )
-    defer notedb.Close()
-
-    if err != nil {
-        panic("failed to connect database")
-    }
-    // Migrate the schema
-    notedb.AutoMigrate(&Note{})
-
-
-    //------------------------------
-    settingdb, err := gorm.Open( useDBMSName , settingDBName )
-    defer settingdb.Close()
-	settingdb.LogMode(true)
-
-    if err != nil {
-        panic("failed to connect database2")
-    }
-
-    var setting Setting
-
-    setting.Name = newNoteName
-    setting.Address = newNoteAddress
-
-
-    // INSERTを実行
-    settingdb.Create(&setting)
-
-
-    log.Println("ノート追加 終了")
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-    return nil
-    
-} //--------------------------------------------
-
-
-
-
-
-//HandleAddPagePost は /hello のPost時のHTMLデータ生成処理を行います。
-func HandleAddPagePost(c echo.Context) error {
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-    log.Println("ページ追加 開始")
-
-    //指定したディレクトリにDBファイルを作成
-    noteAddress   := c.FormValue("note_address")
-
-    notedb, err := gorm.Open( useDBMSName , noteAddress + directorySeparator +  noteDBName )
-    if err != nil {
-        panic("failed to connect database")
-    }
-    notedb.AutoMigrate(&Note{})
-
-    var note Note
-
-    note.PageTitle = ""
-    note.PageBody  = ""
-
-    // INSERTを実行
-    notedb.Create(&note)
-
-    HandleUpdateNoteFromPage(noteAddress) 
-
-    log.Println("ページ追加 終了")
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-
-    return nil
-
-} //--------------------------------------------
-
-
-
 
 
 func checkConfig() {
@@ -453,7 +277,7 @@ func checkConfig() {
     // 設定DB読み込み
     _, err := os.Stat(settingDBName)
     if err != nil {
-        MakeSettingDb()
+        MakeSettingDB()
     }
 
     // 共有ファイルの場所情報を取得
@@ -471,31 +295,6 @@ func checkConfig() {
 
 
 
-// ===============================================
-
-// MakeSettingDb は設定データベースの初期化
-func MakeSettingDb() {
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-    log.Println("設定DB作成 開始")
-
-    file, err := os.OpenFile( settingDBName , os.O_WRONLY|os.O_CREATE, 0666 )
-    if err != nil {
-        //エラー処理
-        log.Fatal(err)
-    }
-    defer file.Close()
-    db, err := gorm.Open( useDBMSName , settingDBName )
-    if err != nil {
-        panic("failed to connect database")
-    }
-    // Migrate the schema
-    db.AutoMigrate(&Setting{})
-
-    log.Println("設定DB作成 終了")
-    log.Println("==============================================================================" )
-    log.Println("==============================================================================" )
-}
 
 // 各HTMLテンプレートに共通レイアウトを適用した結果を保存します（初期化時に実行）。
 func loadTemplates() {
@@ -504,90 +303,7 @@ func loadTemplates() {
     templates["loadpage"]       = template.Must(template.ParseFiles(baseTemplate, "templates/loadpage.html"))
 } //--------------------------------------------
 
-func getSettingData() string {
-    // ノート情報の取得
-    settingdb, err := gorm.Open( useDBMSName , settingDBName )
-	// DBログモードon
-	settingdb.LogMode(true)
 
-    defer settingdb.Close()
-
-    if err != nil {
-        panic("failed to connect database")
-    }
-
-    var count = 0
-    settingdb.Table("settings").Count(&count)
-    
-    //設定DBの中にレコードがなければ処理をスキップ
-
-    if count == 0 { 
-        // handle the error    
-        returnmap := map[string]string{
-            "key0": "1",
-            "key1": "",
-            "key2": "",
-        }
-        jsonreturnmap, err := json.Marshal(returnmap)
-        if err != nil {
-            panic("not convert json array")
-        }
-    
-        stringjsonreturnmap := string(jsonreturnmap)
-        return stringjsonreturnmap
-
-        // return nil
-    }
-    
-    var setting []Setting
-    settingdb.Table("settings").Order("updated_at desc").Find(&setting)
-
-    var data2 = ReturnValue{}
-    var DataSetList = DataSet{}
-    data2.Key0 = "0"
-
-    for _ ,value := range setting {
-
-        NoteDbAddress := value.Address + directorySeparator + noteDBName
-
-        notedb, err := gorm.Open( useDBMSName , NoteDbAddress )
-        notedb.LogMode(true)
-        defer notedb.Close()
-    
-        if err != nil {
-            panic("failed to connect database")
-        }
-        NoteList := []Note{}
-    
-        notedb.Table("notes").Order("updated_at desc").Find(&NoteList)
-    
-        DataSetList.NoteDbID = value.ID
-        DataSetList.NoteDbName = value.Name
-        DataSetList.NoteDbAddress = value.Address
-        DataSetList.NoteDbUpdateTime = value.UpdatedAt 
-        DataSetList.List = NoteList 
-
-        data2.Key1 = append(data2.Key1, DataSetList)
-    
-    }
-
-    jsonreturnmap, err := json.Marshal(data2)
-    if err != nil {
-        panic("not convert json array")
-    }
-    stringjsonreturnmap := string(jsonreturnmap)
-
-    return stringjsonreturnmap
-} //--------------------------------------------
-
-// HandleLoadPageGet は /hello_form のGet時のHTMLデータ生成処理を行います。
-func HandleLoadPageGet(c echo.Context) error {
-    // return nil
-
-    returnjson := getSettingData()
-    return c.Render(http.StatusOK, "loadpage", returnjson)
-
-} //--------------------------------------------
 
 //--------------------------------------------
 func endProcess() {
---
+++ model.go
@@ -0,0 +1,334 @@
+package main
+import (
+
+	"encoding/json"
+	"github.com/jinzhu/gorm"
+    _ "github.com/mattn/go-sqlite3"
+    "os"
+    "log"
+    "strconv"
+
+)
+
+// getData →ノート情報、ページ情報を取得
+func getData() string {
+    // ノート情報の取得
+    settingdb, err := gorm.Open( useDBMSName , settingDBName )
+	// DBログモードon
+	settingdb.LogMode(true)
+
+    defer settingdb.Close()
+
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    var count = 0
+    settingdb.Table("settings").Count(&count)
+    
+    //設定DBの中にレコードがなければ処理をスキップ
+
+    if count == 0 { 
+        // handle the error    
+        returnmap := map[string]string{
+            "key0": "1",
+            "key1": "",
+            "key2": "",
+        }
+        jsonreturnmap, err := json.Marshal(returnmap)
+        if err != nil {
+            panic("not convert json array")
+        }
+    
+        stringjsonreturnmap := string(jsonreturnmap)
+        return stringjsonreturnmap
+
+        // return nil
+    }
+    
+    var setting []Setting
+    settingdb.Table("settings").Order("updated_at desc").Find(&setting)
+
+    var data2 = ReturnValue{}
+    var DataSetList = DataSet{}
+    data2.Key0 = "0"
+
+    for _ ,value := range setting {
+
+        NoteDbAddress := value.Address + directorySeparator + noteDBName
+
+        notedb, err := gorm.Open( useDBMSName , NoteDbAddress )
+        notedb.LogMode(true)
+        defer notedb.Close()
+    
+        if err != nil {
+            panic("failed to connect database")
+        }
+        NoteList := []Note{}
+    
+        notedb.Table("notes").Order("updated_at desc").Find(&NoteList)
+    
+        DataSetList.NoteDbID = value.ID
+        DataSetList.NoteDbName = value.Name
+        DataSetList.NoteDbAddress = value.Address
+        DataSetList.NoteDbUpdateTime = value.UpdatedAt 
+        DataSetList.List = NoteList 
+
+        data2.Key1 = append(data2.Key1, DataSetList)
+    
+    }
+
+    jsonreturnmap, err := json.Marshal(data2)
+    if err != nil {
+        panic("not convert json array")
+    }
+    stringjsonreturnmap := string(jsonreturnmap)
+
+    return stringjsonreturnmap
+} //--------------------------------------------
+
+// addNote →ノート情報を追加
+func addNote( argMap map[string]string ) error {
+
+	//指定したディレクトリにDBファイルを作成
+	newNoteName    := argMap["newNoteName"]
+	newNoteAddress := argMap["newNoteAddress"]
+	
+    notefullAddress := newNoteAddress + directorySeparator + noteDBName
+
+    file, err := os.OpenFile( notefullAddress , os.O_WRONLY|os.O_CREATE, 0666 )
+    if err != nil {
+        //エラー処理
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    notedb, err := gorm.Open( useDBMSName , notefullAddress )
+    defer notedb.Close()
+
+    if err != nil {
+        panic("failed to connect database")
+    }
+    // Migrate the schema
+    notedb.AutoMigrate(&Note{})
+
+
+    //------------------------------
+    settingdb, err := gorm.Open( useDBMSName , settingDBName )
+    defer settingdb.Close()
+	settingdb.LogMode(true)
+
+    if err != nil {
+        panic("failed to connect database2")
+    }
+
+    var setting Setting
+
+    setting.Name = newNoteName
+    setting.Address = newNoteAddress
+
+
+    // INSERTを実行
+    settingdb.Create(&setting)
+
+
+	return nil
+} //--------------------------------------------
+
+// addPage →ページ情報を追加
+func addPage(argMap map[string]string ) error {
+
+	//指定したディレクトリにDBファイルを作成
+	noteAddress    := argMap["noteAddress"]
+
+
+    notedb, err := gorm.Open( useDBMSName , noteAddress + directorySeparator +  noteDBName )
+    if err != nil {
+        panic("failed to connect database")
+    }
+    notedb.AutoMigrate(&Note{})
+
+    var note Note
+
+    note.PageTitle = ""
+    note.PageBody  = ""
+
+    // INSERTを実行
+    notedb.Create(&note)
+
+    updateNoteFromPage(noteAddress) 
+
+
+	return nil
+} //--------------------------------------------
+
+// updateNote →ノート情報を更新
+func updateNote( argMap map[string]string  ) error {
+//	
+	// __________________________________
+	// ポスト内容取得
+    newNoteName :=argMap[ "newNoteName"]
+    postNoteID  :=argMap[ "postNoteID"]
+
+    //------------------------------
+    settingdb, err := gorm.Open( useDBMSName , settingDBName )
+    defer settingdb.Close()
+	settingdb.LogMode(true)
+
+    if err != nil {
+        panic("failed to connect database2")
+    }
+
+    var setting Setting
+    // noteID := int(noteID)
+    var noteID int
+    noteID, _ = strconv.Atoi(postNoteID)
+
+    settingdb.Where("id = ?", noteID ).First(&setting)
+
+    setting.Name = newNoteName
+    settingdb.Save(&setting)
+
+	return nil
+} //--------------------------------------------
+
+// updatePage →ページ情報を更新
+func updatePage( argMap map[string]string ) error {
+//	 
+
+	// __________________________________
+	// ポスト内容取得
+    noteAddress := argMap["noteAddress"]
+    pageID      := argMap["pageID"]     
+    pageTitle   := argMap["pageTitle"]  
+    pageBody    := argMap["pageBody"]   
+
+    if pageTitle == "" {
+        pageTitle =  " "
+    }
+
+    if pageBody == "" {
+        pageBody =  " "
+    }
+    
+    notedb, err := gorm.Open("sqlite3", noteAddress + directorySeparator + noteDBName )
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    defer notedb.Close()
+	notedb.LogMode(true)
+
+
+	// __________________________________
+	// DB内容取得
+    NoteList := []Note{}
+
+	notedb.Model(&NoteList).Where("id = ?", pageID).Update(&Note{PageTitle: pageTitle, PageBody: pageBody})
+
+
+    
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+    updateNoteFromPage(noteAddress)
+
+	return nil
+} //--------------------------------------------
+
+// deleteNote →ノート情報を追加
+func deleteNote( argMap map[string]string ) error {
+
+	// __________________________________
+	// ポスト内容取得
+    postNoteID  := argMap["postNoteID"]
+
+    //------------------------------
+    settingdb, err := gorm.Open( useDBMSName , settingDBName )
+    defer settingdb.Close()
+	settingdb.LogMode(true)
+
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    var setting Setting
+    var noteID int
+    noteID, _ = strconv.Atoi(postNoteID)
+
+    settingdb.Where("id = ?", noteID ).Delete(&setting)
+
+	return nil
+} //--------------------------------------------
+
+// deletePage →ページ情報を追加
+func deletePage( argMap map[string]string) error {
+//	argMap map[string]string 
+
+	// __________________________________
+	// ポスト内容取得
+    noteAddress := argMap["noteAddress"]
+    pageID      := argMap["pageID"]     
+
+    notedb, err := gorm.Open("sqlite3", noteAddress + directorySeparator + noteDBName )
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    defer notedb.Close()
+	notedb.LogMode(true)
+
+	// __________________________________
+	// DB内容取得
+    notedb.Where("id = ?", pageID).Delete(&Note{})
+    
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	return nil
+} //--------------------------------------------
+
+// updateNoteFromPage のコメントアウト
+func updateNoteFromPage(noteAddress string)  {
+
+    settingdb, err := gorm.Open("sqlite3", settingDBName )
+    defer settingdb.Close()
+	settingdb.LogMode(true)
+
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    var setting Setting
+
+    settingdb.Where("address = ?", noteAddress ).First(&setting)
+
+    setting.Address = noteAddress
+    settingdb.Save(&setting)
+
+
+
+} //--------------------------------------------
+
+
+// ===============================================
+
+// MakeSettingDb は設定データベースの初期化
+func MakeSettingDB() {
+
+    file, err := os.OpenFile( settingDBName , os.O_WRONLY|os.O_CREATE, 0666 )
+    if err != nil {
+        //エラー処理
+        log.Fatal(err)
+    }
+    defer file.Close()
+    db, err := gorm.Open( useDBMSName , settingDBName )
+    if err != nil {
+        panic("failed to connect database")
+    }
+    // Migrate the schema
+    db.AutoMigrate(&Setting{})
+
+}
